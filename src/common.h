72fc3a0dadedfa4914d3e6520beb06f99fe8b4e0
Censacrof <galisifrancesco@gmail.com>
2019-12-18 01:16:38 +0100

    levati include inutili da common.h

--- src/common.h
+++ src/common.h
@@ -1,12 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-
 #define DEFAULT_SERVER_PORT "1234"
 
 void die(const char *msg);
