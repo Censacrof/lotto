1fd2ee6930decf0727df2b4a9d48a49c76e423cc
Censacrof <galisifrancesco@gmail.com>
2020-01-31 23:41:24 +0100

    fixato bug (i session id generati erano di 9 caratteri invece che di 10)

--- src/server/commands/login.c
+++ src/server/commands/login.c
@@ -60,7 +60,7 @@ int login(int client_sock, const char *client_addr_str, int nargs, char *args[])
     {
         session.id[i] = charset[rand() % (charsetlen + 1)];
     }
-    session.id[SESSIONID_LEN - 1] = '\0';
+    session.id[SESSIONID_LEN] = '\0';
     
     // invio il sessionid al client
     send_response(client_sock, SRESP_OK, session.id);
